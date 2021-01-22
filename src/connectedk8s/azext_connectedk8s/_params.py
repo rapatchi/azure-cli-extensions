2b093ac82fce083872f4e37873f0c452551b1dd5
Atharva Mulmuley <atharvam@microsoft.com>
2021-01-22 14:44:29 +0530

    removed get-credentials from params.py

--- src/connectedk8s/azext_connectedk8s/_params.py
+++ src/connectedk8s/azext_connectedk8s/_params.py
@@ -54,13 +54,6 @@ def load_arguments(self, _):
         c.argument('kube_config', options_list=['--kube-config'], help='Path to the kube config file.')
         c.argument('kube_context', options_list=['--kube-context'], help='Kubconfig context from current machine.')
 
-    with self.argument_context('connectedk8s get-credentials') as c:
-        c.argument('cluster_name', options_list=['--name', '-n'], id_part='name', help='The name of the connected cluster.')
-        c.argument('context_name', options_list=['--context'], help='If specified, overwrite the default context name.')
-        c.argument('overwrite_existing', options_list=['--overwrite-existing'], help='Overwrite any existing cluster entry with the same name.')
-        c.argument('token', options_list=['--auth-token'], help='Client authentication token for non-AAD scenario.')
-        c.argument('path', options_list=['--file', '-f'], type=file_type, completer=FilesCompleter(), default=os.path.join(os.path.expanduser('~'), '.kube', 'config'), help="Kubernetes configuration file to update. If not provided, updates the file '~/.kube/config'. Use '-' to print YAML to stdout instead.")
-
     with self.argument_context('connectedk8s proxy') as c:
         c.argument('cluster_name', options_list=['--name', '-n'], id_part='name', help='The name of the connected cluster.')
         c.argument('token', options_list=['--auth-token'], help='Client authentication token for non-AAD scenario.')
